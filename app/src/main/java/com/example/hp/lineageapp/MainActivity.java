3c271a85c094f311c5035d3827df66446b837e6e
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-11 23:26:27 +0100

    Handle Navigation clicks
    Implement OnNavigationItemSelectedListener interface and attach it to the NavigationView by calling setNavigationItemSelectedListener()
    
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

--- app/src/main/java/com/example/hp/lineageapp/MainActivity.java
+++ app/src/main/java/com/example/hp/lineageapp/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.hp.lineageapp;
 
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,20 @@ public class MainActivity extends AppCompatActivity {
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                // set item as selected to persist highlight
+                menuItem.setChecked(true);
+                // close drawer when an item is tapped
+                mDrawerLayout.closeDrawers();
+
+                // TODO: add code to update UI when an item is tapped
+                return true;
+            }
+        });
     }
 }
