6e859a9d88bd0e593266f469fe7da6317f0352f1
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-11 23:19:17 +0100

    Add nav drawer button
    The app bar home button is enabled by calling the method setDisplayHomeAsUpEnabled.
    Change the home to use the drawable menu icon by calling setHomeAsUpIndicator(int)
    
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

--- app/src/main/java/com/example/hp/lineageapp/MainActivity.java
+++ app/src/main/java/com/example/hp/lineageapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hp.lineageapp;
 
 import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -15,5 +16,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(Color.WHITE);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
     }
 }
